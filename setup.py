f5e8914fab1d6403f3c1a24e302e888d9f722fed
b4stien <bastien@pectoribus.net>
2013-06-02 14:06:44 +0200

    Fix version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mozbase',
-    version='0.2.0',
+    version='0.2.1',
     packages=['mozbase'],
     test_suite='tests',
     install_requires=['SQLAlchemy', 'voluptuous'],
