76223d40eccbe6d4ca230952e4d7a4071d27ca2b
b4stien <bastien@pectoribus.net>
2013-06-20 18:06:08 +0200

    Bump version to 0.3.0dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mozbase',
-    version='0.2.3',
+    version='0.3.0dev',
     packages=['mozbase'],
     test_suite='tests',
     install_requires=['SQLAlchemy', 'voluptuous'],
