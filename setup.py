f32c369a6e59f69bfe6e5c75460d7e554fcc2d3d
b4stien <bastien@pectoribus.net>
2013-06-23 23:46:51 +0200

    Bump to 0.3.0beta

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mozbase',
-    version='0.3.0dev',
+    version='0.3.0beta',
     packages=['mozbase'],
     test_suite='tests',
     install_requires=['SQLAlchemy', 'voluptuous', 'dogpile.cache'],
