f0599d537d06db3b68f3dbfc959b9eebf6618153
b4stien <bastien@pectoribus.net>
2013-06-26 15:50:35 +0200

    Fix install requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     version='0.3.0beta',
     packages=['mozbase'],
     test_suite='tests',
+    install_requires=[
+        'SQLAlchemy==0.8.1',
+        'voluptuous==0.7.3',
+        'dogpile.cache==0.4.3'],
     author='Bastien GANDOUET',
     author_email="bastien@mozaiqu.es"
 )
