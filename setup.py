8be9673908f0dacaa4bf5545478511d7fbc10616
b4stien <bastien@pectoribus.net>
2013-07-03 21:11:28 +0200

    This is 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='mozbase',
-    version='0.3.0beta',
+    version='0.3.0',
     packages=['mozbase'],
     test_suite='tests',
     install_requires=[
