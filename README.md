f3b62665cf051619049002e993d7bcb7f5852c5c
b4stien <bastien@pectoribus.net>
2013-09-14 12:36:24 +0200

    This is 0.3.8 (adapt_dict and voluptuous update).

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Dependencies
 ------------
 
 * [SQLAlchemy](http://hg.sqlalchemy.org/sqlalchemy) (0.8.2)
-* [Voluptuous](https://github.com/alecthomas/voluptuous) (0.7.2)
+* [Voluptuous](https://github.com/alecthomas/voluptuous) (0.8.1)
 * [dogpile.cache](http://dogpilecache.readthedocs.org/en/latest/) (0.5.0)
 
 Intended to be used
--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name='mozbase',
-    version='0.3.7',
+    version='0.3.8',
     packages=['mozbase'],
     test_suite='tests',
     install_requires=[
         'SQLAlchemy==0.8.2',
-        'voluptuous==0.7.2',
+        'voluptuous==0.8.1',
         'dogpile.cache==0.5.0'],
     author='Bastien GANDOUET',
     author_email="bastien@mozaiqu.es"
