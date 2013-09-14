538e449e280affd717c01bc25d5a587b2d6bfaf6
Raphael Kolm <raphael.kolm@gmail.com>
2013-09-14 11:20:03 +0200

    util.validation.adapt_dict method

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from voluptuous import Schema, MultipleInvalid
+from voluptuous import Schema, MultipleInvalid, Required
 
-from mozbase.util.validation import Email, Choice
+from mozbase.util.validation import Email, Choice, adapt_dict
 
 
 class TestModelBase(unittest.TestCase):
@@ -25,6 +25,33 @@ class TestModelBase(unittest.TestCase):
         with self.assertRaises(MultipleInvalid):
             schema('c')
 
+
+class TestValidationAdaptDict(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.input_dict = {
+            'to_keep': 'dummy',
+            'to_remove': 'dummy',
+            'to_make_required': 'dummy'
+        }
+
+    def test_keep(self):
+        output_dict = adapt_dict(self.input_dict, keep=['to_keep'])
+        self.assertEqual(output_dict, {'to_keep': 'dummy'})
+
+    def test_remove(self):
+        output_dict = adapt_dict(self.input_dict, remove=['to_remove'])
+        self.assertEqual(output_dict, {'to_keep': 'dummy',
+                                       'to_make_required': 'dummy'})
+
+    # Does not work because Required('to_make_required') cannot be used as a
+    # key to fetch in output_dict...
+    #
+    #def test_make_required(self):
+    #    output_dict = adapt_dict(self.input_dict,
+    #                             make_required=['to_make_required'])
+    #    self.assertTrue(Required('to_make_required') in output_dict)
 
 if __name__ == '__main__':
     unittest.main()
