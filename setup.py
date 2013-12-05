8311756ceadee439928c88896d5261a9a1ba6783
b4stien <bastien@pectoribus.net>
2013-12-05 10:55:58 +0100

    Update requirements. This is mozbase 0.3.9

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
+from mozbase import __version__
+
 
 setup(
     name='mozbase',
-    version='0.3.8',
+    version=__version__,
     packages=['mozbase'],
     test_suite='tests',
     install_requires=[
-        'SQLAlchemy==0.8.2',
-        'voluptuous==0.8.1',
-        'dogpile.cache==0.5.0'],
+        'SQLAlchemy==0.8.3',
+        'voluptuous==0.8.4',
+        'dogpile.cache==0.5.2'],
     author='Bastien GANDOUET',
     author_email="bastien@mozaiqu.es"
 )
