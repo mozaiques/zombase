fea2ca4a87efc2a2a7c674d3171a2fc421ff4fa7
Bastien Gandouet <bastien@mozaiqu.es>
2014-03-24 21:14:21 +0100

    This is mozbase 0.4.0dev

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 
 setup(
     name='mozbase',
-    version='0.3.10',
+    version='0.4.0dev',
     packages=find_packages(),
     test_suite='tests',
     install_requires=[
-        'SQLAlchemy==0.9.2',
-        'voluptuous==0.8.4',
-        'dogpile.cache==0.5.3',
+        str(ir.req) for ir in parse_requirements('requirements.txt')
     ],
     author='Bastien GANDOUET',
     author_email="bastien@mozaiqu.es"
