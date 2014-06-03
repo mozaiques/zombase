999f58aa0f2019ca84d0b164c0e9770b026c95e4
Bastien Gandouet <bastien@mozaiqu.es>
2014-06-03 10:00:10 +0200

    This is actually mozbase 0.4.4...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pip.req import parse_requirements
 
 setup(
     name='mozbase',
-    version='0.4.3dev',
+    version='0.4.4',
     packages=find_packages(),
     test_suite='tests',
     install_requires=[
