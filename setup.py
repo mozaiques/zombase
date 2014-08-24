a496765468beea6028821e98224c3bd0b768825b
Bastien Gandouet <bastien@mozaiqu.es>
2014-08-24 19:03:59 +0200

    This is mozbase 0.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pip.req import parse_requirements
 
 setup(
     name='mozbase',
-    version='0.5.2dev',
+    version='0.5.2',
     packages=find_packages(),
     test_suite='tests',
     install_requires=[
