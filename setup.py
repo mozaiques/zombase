162787006da8b6701ef3d197f1e102e366509e93
Bastien Gandouet <bastien@mozaiqu.es>
2014-09-16 16:56:23 +0200

    This is zombase 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zombase',
-    version='0.0.1dev',
+    version='0.1.0',
     packages=find_packages(),
     test_suite='tests',
     install_requires=[
