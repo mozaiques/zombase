b44f2b7cd89a9fc5fee00080d9bc172135db17cc
Bastien Gandouet <bastien@mozaiqu.es>
2014-09-17 13:51:13 +0200

    This is zombase 0.2.3dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zombase',
-    version='0.2.2',
+    version='0.2.3dev',
     license='MIT',
     author='Bastien GANDOUET',
     author_email="bastien@mozaiqu.es",
