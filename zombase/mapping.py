7a48c9d1563fbe2bf54ed760dab5ca55f5e677f7
Bastien Gandouet <bastien@mozaiqu.es>
2015-10-27 19:00:55 +0100

    BREAKING. Move some parts out of the workers, change some logic.

---
+++ zombase/mapping.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import inspect
+import six
+
+from voluptuous import Schema
+
+from zombase.database import MetaBase
+
+
+def update(sqla_obj, schema, **kwargs):
+    """Update an `instance`. Return False if there is no update and
+    True otherwise.
+
+    Do not raise error if too many arguments are given.
+
+    Do not commit the database session.
+
+    Keyword arguments:
+        sqla_obj -- SQLAlchemy object to update
+        schema -- voluptuous schema to perform data validation
+
+    """
+    if not isinstance(schema, Schema):
+        raise AttributeError('`schema` must be a voluptuous schema.')
+
+    # Explicitely cast to string properties which come from schema
+    # to deal with `voluptuous.Required` stuff.
+    schema_keys = set([str(k) for k in schema.schema])
+
+    obj_current_dict = {
+        k: getattr(sqla_obj, k) for k in schema_keys
+        if not getattr(sqla_obj, k) is None
+    }
+    obj_update_dict = obj_current_dict.copy()
+
+    to_update = schema_keys.intersection(kwargs.keys())
+
+    for item in to_update:
+        obj_update_dict[item] = kwargs[item]
+
+    obj_update_dict = schema(obj_update_dict)
+
+    for item in to_update:
+        setattr(sqla_obj, item, obj_update_dict[item])
+
+    return obj_update_dict != obj_current_dict
+
+
+def resolve_id(build_query, a_dict, schema, allow_none_id=False):
+    """Return a dict fulfilled with the missing objects according to
+    the given dict (in `a_dict`) and `schema`.
+
+    Will fetch `sqla_obj` if `sqla_obj_id` (or `sqla_obj_uuid`) is in
+    the dict keys, if `sqla_obj` is accepted by the schema and is
+    supposed to be a SQLAlchemy object.
+
+    Additionnally if `sqla_obj_id` is not in the schema, it will be
+    removed from the dict.
+
+    If `allow_none_id` is true, passing `sqla_obj_id` with value
+    'None' or '""' (empty string) will result in setting `sqla_obj`
+    to 'None'.
+
+    Arguments:
+        build_query -- function which will be given a SQLAlchemy mapping
+                       and must return an appropriate query
+        a_dict -- base dictionnary
+        schema -- reference voluptuous schema
+        allow_none_id -- bool (see above)
+
+    """
+    _a_dict = a_dict.copy()
+
+    for key, sqla_map in six.iteritems(schema.schema):
+        if key in _a_dict.keys():
+            continue
+
+        if not inspect.isclass(sqla_map) or not issubclass(sqla_map, MetaBase):
+            continue
+
+        key_id = '{}_id'.format(key)
+        key_uuid = '{}_uuid'.format(key)
+
+        if key_id in a_dict and key_uuid in a_dict:
+            raise Exception('`_resolve_id()` has been called with '
+                            'both an `object_id` and an '
+                            '`object_uuid`.')
+
+        elif key_id in a_dict:
+            val_id = _a_dict.pop(key_id)
+            if not val_id and allow_none_id:
+                val = None
+
+            else:
+                val = build_query(sqla_map).filter(sqla_map.id == val_id).one()
+
+            _a_dict[str(key)] = val
+
+        elif key_uuid in a_dict:
+            val_uuid = _a_dict.pop(key_uuid)
+            if not val_uuid and allow_none_id:
+                val = None
+
+            else:
+                val = build_query(sqla_map)\
+                    .filter(sqla_map.uuid == val_uuid).one()
+
+            _a_dict[str(key)] = val
+
+    return _a_dict
--- zombase/worker.py
+++ zombase/worker.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-import inspect
-
-from voluptuous import Schema
-
 import six
 
-from zombase.database import MetaBase
+from zombase.errors import ZombaseRuntimeError
 
 
 class RawWorker(object):
@@ -32,191 +28,98 @@ class SupervisedWorker(RawWorker):
         setattr(self, foreman_name, foreman)
 
 
-class ObjectManagingWorker(SupervisedWorker):
+class MappingManagingWorker(SupervisedWorker):
     """Worker intended to be used inside a foreman, to take care of a
-    specific sqlalchemy mapping.
+    specific SQLAlchemy mapping.
 
     """
 
-    def __init__(self, foreman=None, foreman_name=None, managed_object=None,
-                 managed_object_name=None, id_type=None):
+    def __init__(self, foreman=None, foreman_name=None, managed_sqla_map=None,
+                 managed_sqla_map_name=None, id_type=None):
         SupervisedWorker.__init__(self, foreman, foreman_name)
 
-        self._object = managed_object
-        self._object_name = managed_object_name
+        self._sqla_map = managed_sqla_map
+        self._sqla_map_name = managed_sqla_map_name
 
         if id_type and id_type not in ('id', 'uuid'):
             raise AttributeError('Wrong `id_type`.')
-        self._with_id = (id_type == 'id' or hasattr(self._object, 'id'))
-        self._with_uuid = (id_type == 'uuid' or hasattr(self._object, 'uuid'))
+        self._with_id = id_type == 'id' or hasattr(self._sqla_map, 'id')
+        self._with_uuid = id_type == 'uuid' or hasattr(self._sqla_map, 'uuid')
 
-    def _get(self, instance_id=None, instance=None, options=None):
-        """Unified internal get for an object present in `instance_id`
-        or `instance`, whose type is `self._object`.
-
-        Keyword arguments:
-            instance_id -- id (could be a "real" integer id or an uuid)
-                           of the requested object
-            instance -- object (internal use)
-            options -- list of SQLA options to apply to the SQL request
+    def _base_query(self, **kwargs):
+        """Subclasses can override this method to implement custom logic
+        (filtering inactive objects, security features, etc).
 
         """
-        if instance:
-            if not isinstance(instance, self._object):
-                raise AttributeError('`instance` doesn\'t match with the '
-                                     'registered type.')
-            return instance
+        return self._dbsession.query(self._sqla_map)
 
-        elif instance_id:
+    def _get(self, sqla_obj_id=None, sqla_obj=None, options=None):
+        """Unified internal get for a SQLAlchemy object present in
+        `sqla_obj_id` or `sqla_obj`, whose type is `self._sqla_map`.
+
+        Keyword arguments:
+            sqla_obj_id -- id (could be a "real" integer id or an uuid)
+                           of the requested object
+            sqla_obj -- SQLAlchemy object (internal use)
+            options -- list of SQLAchemy options to apply to the SQL
+                       request
+
+        """
+        if sqla_obj:
+            if not isinstance(sqla_obj, self._sqla_map):
+                raise ValueError('`sqla_obj` doesn\'t match with the '
+                                 'registered type.')
+            return sqla_obj
+
+        elif sqla_obj_id:
             if options is None:
                 options = []
-            query = self._dbsession.query(self._object)
+            query = self._base_query()
 
             if self._with_id:
-                query = query.filter(self._object.id == instance_id)
+                query = query.filter(self._sqla_map.id == sqla_obj_id)
             elif self._with_uuid:
-                query = query.filter(self._object.uuid == instance_id)
+                query = query.filter(self._sqla_map.uuid == sqla_obj_id)
             else:
-                raise StandardError('Can\'t determine id field.')
+                raise ZombaseRuntimeError('Can\'t determine id field.')
 
             return query.options(*options).one()
 
         raise TypeError('No criteria provided.')
 
-    def _update(self, instance=None, schema=None, **kwargs):
-        """Update an `instance`. Return False if there is no update and
-        True otherwise.
-
-        Do not raise error if too many arguments are given.
-
-        Do not commit the database session.
-
-        Keyword arguments:
-            instance -- instance to update
-            schema -- voluptuous schema to perform data validation
-
-        """
-        if not instance:
-            raise TypeError('`instance` not provided.')
-
-        if not schema:
-            raise TypeError('`schema` not provided.')
-
-        if not isinstance(schema, Schema):
-            raise AttributeError('`schema` must be a voluptuous schema.')
-
-        # Explicitely cast to string properties which come from schema
-        # to deal with `voluptuous.Required` stuff.
-        schema_keys = set([str(k) for k in schema.schema])
-
-        obj_current_dict = {
-            k: getattr(instance, k) for k in schema_keys
-            if not getattr(instance, k) is None
-        }
-        obj_update_dict = obj_current_dict.copy()
-
-        to_update = schema_keys.intersection(kwargs.keys())
-
-        for item in to_update:
-            obj_update_dict[item] = kwargs[item]
-
-        obj_update_dict = schema(obj_update_dict)
-
-        for item in to_update:
-            setattr(instance, item, obj_update_dict[item])
-
-        return obj_update_dict != obj_current_dict
-
-    def _resolve_id(self, a_dict, schema, allow_none_id=False):
-        """Return a dict fulfilled with the missing objects according to
-        the given dict (in `a_dict`) and `schema`.
-
-        Will fetch `instance` if `instance_id` (or `instance_uuid`) is
-        in the dict keys, if `instance` is accepted by the schema and is
-        supposed to be a sqlalchemy mapping.
-
-        Additionnally if `instance_id` is not in the schema, it will be
-        removed from the dict.
-
-        If `allow_none_id` is true, passing `instance_id` with value
-        'None' or '""' (empty string) will result in setting `instance`
-        to 'None'.
-
-        """
-        _a_dict = a_dict.copy()
-
-        for key, value in six.iteritems(schema.schema):
-            if key in _a_dict.keys():
-                continue
-
-            if not inspect.isclass(value) or not issubclass(value, MetaBase):
-                continue
-
-            key_id = '{}_id'.format(key)
-            key_uuid = '{}_uuid'.format(key)
-
-            if key_id in a_dict and key_uuid in a_dict:
-                raise Exception('`_resolve_id()` has been called with '
-                                'both an `instance_id` and an '
-                                '`instance_uuid`.')
-
-            elif key_id in a_dict:
-                val_id = _a_dict.pop(key_id)
-                if not val_id and allow_none_id:
-                    val = None
-
-                else:
-                    val = self._dbsession.query(value)\
-                        .filter(value.id == val_id).one()
-
-                _a_dict[str(key)] = val
-
-            elif key_uuid in a_dict:
-                val_uuid = _a_dict.pop(key_uuid)
-                if not val_uuid and allow_none_id:
-                    val = None
-
-                else:
-                    val = self._dbsession.query(value)\
-                        .filter(value.uuid == val_uuid).one()
-
-                _a_dict[str(key)] = val
-
-        return _a_dict
-
-    def get(self, instance_id=None, instance=None, options=None, **kwargs):
-        """Unified external get for an object present in `instance_id`
-        or `instance`.
+    def get(self, sqla_obj_id=None, sqla_obj=None, options=None, **kwargs):
+        """Unified external get for an object present in `sqla_obj_id`
+        or `sqla_obj`.
 
         See also `ObjectManagingWorker._get()`.
 
         """
-        if not instance_id and not instance:
-            if not self._object_name:
+        if not sqla_obj_id and not sqla_obj:
+            if not self._sqla_map_name:
                 raise TypeError('No criteria provided.')
 
-            instance_key = '{}'.format(self._object_name)
+            sqla_obj_key = '{}'.format(self._sqla_map_name)
             if self._with_id:
-                instance_id_key = '{}_id'.format(self._object_name)
+                sqla_obj_id_key = '{}_id'.format(self._sqla_map_name)
             elif self._with_uuid:
-                instance_id_key = '{}_uuid'.format(self._object_name)
+                sqla_obj_id_key = '{}_uuid'.format(self._sqla_map_name)
             else:
-                raise StandardError('Can\'t determine id field.')
+                raise ZombaseRuntimeError('Can\'t determine id field.')
 
-            if instance_key in kwargs:
-                instance = kwargs[instance_key]
+            if sqla_obj_key in kwargs:
+                sqla_obj = kwargs[sqla_obj_key]
 
-            elif instance_id_key in kwargs:
-                instance_id = kwargs[instance_id_key]
+            elif sqla_obj_id_key in kwargs:
+                sqla_obj_id = kwargs[sqla_obj_id_key]
 
             else:
                 raise TypeError('No criteria provided.')
 
-        return self._get(instance_id, instance, options)
+        return self._get(sqla_obj_id, sqla_obj, options)
 
     def find(self):
         """Return a query to fetch multiple objects."""
-        return self._dbsession.query(self._object)
+        return self._dbsession.query(self._sqla_map)
 
     def serialize(self, items, **kwargs):
         """Transform the given list of `items` into an easily
