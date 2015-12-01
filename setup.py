6916d6052d9d217360706bef0d027aa7e1cee60f
Bastien Gandouet <bastien@mozaiqu.es>
2015-12-01 14:23:33 +0100

    This is zombase 0.6.3dev

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as r_file:
 
 setup(
     name='zombase',
-    version='0.6.2',
+    version='0.6.3dev',
     license='MIT',
     author='Bastien Gandouet',
     author_email='bastien@mozaiqu.es',
